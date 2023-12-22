2981bfadbf8033eb9a9fed8aa3d63cea9cdd870d
masud90895 <masudhossainmbs129@gmail.com>
2023-12-22 10:38:01 +0600

    Refactor Footer component to include navigation and styling

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -1,10 +1,109 @@
-import React from 'react';
+import { navigation } from "@/constants/FooterList";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className="bg-white" aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="space-y-8">
+            {/* NFTERS */}
+
+            <h3 className="text-2xl font-bold text-indigo-600">NFTERS</h3>
+
+            <p className="text-sm leading-6 text-gray-600">
+              The World&apos;s first and largest digital Marketplace for crypto
+              collectibles and non fungible taken (NFTs).Buy,sell and discover
+              exclusive digital items{" "}
+            </p>
+            <div className="flex space-x-6">
+              {navigation.social.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  Market Place
+                </h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.solutions.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  My Account
+                </h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.support.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="mt-10 xl:mt-0">
+              <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                Stay In The Loop
+              </h3>
+              <p className="mt-2 text-sm leading-6 text-gray-600">
+               Join our mailing list to stay up to date on the latest news and
+                announcements.
+              </p>
+              <form className="mt-6 sm:flex sm:max-w-md">
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  autoComplete="email"
+                  required
+                  className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                  placeholder="Enter your email"
+                />
+                <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+                  <button
+                    type="submit"
+                    className="flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
